9dd32e796acec3bf3581ffb13047f90de2c0812c
Stackbit Code Editor <projects@stackbit.com>
2021-05-31 11:50:18 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -79,7 +79,7 @@ export default class HeroSection extends React.Component {
                                     'text-right': alignX === 'right'
                                 })}
                             >
-                                {title && <h1 className="hero__title">{title + 1111}</h1>}
+                                {title && <h1 className="hero__title">{title + 2222}</h1>}
                                 {subtitle && <p className="hero__subtitle">{subtitle}</p>}
                                 {content && <div className="hero__copy">{markdownify(content)}</div>}
                                 {!_.isEmpty(actions) && (
